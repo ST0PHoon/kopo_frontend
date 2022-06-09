ae544542072c83634ba50c04615c28af0b075c8d
ST0PHoon <codingnb306@naver.com>
2022-06-09 13:02:12 +0900

    갓주현의 방법

--- J/Word.java
+++ J/Word.java
@@ -1,33 +1,20 @@
 package junsuk5;
 
 public class Word {
-	
+
 	public static void main(String[] args) {
 		Word word = new Word("Hello World");
 		System.out.println(word.isVowel(4));
 	}
-	
+
 	private String letters;
 
 	public Word(String letters) {
 		this.letters = letters.toLowerCase();
 	}
 
-	// i번째 글자가 모음인가  a i u e o
+	// i번째 글자가 모음인가 a i u e o
 	public boolean isVowel(int i) {
-		String[] collectionList = {"a", "i", "u", "e", "o"};
-		int count = 0;
-		
-		for (String item : collectionList) {
-			if (letters.substring(i, i + 1).equals(item)) {
-				count++;
-			}
-		}
-		
-		if (count > 0) {
-			return true;
-		} else {
-			return false;
-		}
+		return "aiueo".contains(letters.substring(i, i + 1));
 	}
 }
